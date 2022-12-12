f32493e408f5b24e210f21a1a5273da70a2fd322
Ardit Rrushaj <arditrrushaj@gmail.com>
2022-12-12 15:03:13 -0600

    blah

--- app.py
+++ app.py
@@ -1,1 +1,56 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, redirect, render_template, flash, get_flashed_messages, jsonify, request
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+@app.get("/api/cupcakes")
+def get_all_cupcakes():
+    """Get data about all cupcakes."""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.get("/api/cupcakes/<cupcake_id>")
+def get_cupcake(cupcake_id):
+    """Get data about single cupcake"""
+
+    cupcake = Cupcake.query.get(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+@app.post("/api/cupcakes")
+def create_cupcake():
+    """
+    Create a cupcake with flavor, size, rating and image data
+    from the body of the request.
+    """
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
+
--- models.py
+++ models.py
@@ -1,1 +1,39 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://tinyurl.com/demo-cupcake"
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Cupcake."""
+
+    ___tablename___ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+    def serialize(self):
+        """Serialize to dictionary."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image
+        }
+
+
+
